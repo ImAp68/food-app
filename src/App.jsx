d319fefe5ae4fe9b3c3058e2fd93dec01e055b89
imap68 <iman.azizpour68@gmail.com>
2024-03-27 21:02:07 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  console.log("FFFFF");
+  return (
+    <>
+      <div className="w-screen h-auto flex  flex-col">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
